98f21a9a6046e585a8cfb154caff6e854643bf62
James Gomez <james.gomez320@yahoo.com>
2019-04-28 00:23:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,20 @@
 # Write a speak_to_grandma method.
+greeting = "Hi Nana, how are you?"
+first = "Hi!"
+food = "WHAT DID YOU EAT TODAY?"
+confused = "WHAT!?"
+heart = "I LOVE YOU GRANDMA!"
+second_time = "HUH?! SPEAK UP, HONEY!"
 
-# Whatever you say to grandma, she should respond with
+ def speak_to_grandma
+   if speak_to_grandma == heart
+     print "I LOVE YOU TOO PUMPKIN!"
+   else if speak_to_grandma_ == greeting
+     print "HUH?! SPEAK UP, SONNY!"
+   
+
+
+whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
